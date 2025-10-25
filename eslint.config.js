f58a1929f9cc0d6746455f929419211d65244d61
中村郁弥 <n.fmy.0526@iclooud.com>
2025-10-25 11:54:32 +0900

    esLintとPrettierの導入

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+import pluginPrettier from 'eslint-plugin-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,10 +16,20 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      // Prettier設定を一番最後に追加
+      prettier,
     ],
+    plugins: {
+      // Prettierプラグインを登録
+      prettier: pluginPrettier,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      // Prettierの整形違反を警告として出す
+      'prettier/prettier': 'warn',
+    },
   },
-])
+]);
